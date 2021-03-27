3a6e12c89e41f2d060d11401e9e3a673a6d243cb
Alex Szabó <salexgy@gmail.com>
2021-03-27 15:51:55 +0100

    recursive mode for directories

--- src/ned2k.nim
+++ src/ned2k.nim
@@ -1,25 +1,65 @@
 import
-  std/os,
+  std/[os, parseopt],
   ned2kpkg/ed2k
 
 
+
 proc printHelp() =
-  echo "usage: ned2k PATH"
+  echo "usage: ned2k [OPTION]... [PATH]..."
   echo "PATH must be a valid file or directory."
-  quit()
+  echo ""
+  echo "Options"
+  echo "  -r, --recursive       compute the ed2k links for files in the given directory recursively"
+  echo "  -h, --help            display this help screen"
 
-
-if paramCount() < 1:
-  printHelp()
-
-let
-  path = paramStr(1)
-
-if fileExists(path):
+proc handleFile(path: string) =
   echo ed2kLink(path)
-elif dirExists(path):
-  for p in path.walkDir:
-    if p.kind == pcFile or p.kind == pcLinkToFile:
-      echo ed2kLink(p.path)
-else:
+
+proc handleDir(path: string, isRecursive: bool) =
+  let
+    followFilter =
+      if isRecursive:
+        {pcDir, pcLinkToDir}
+      else:
+        {}
+  for p in path.walkDirRec({pcFile, pcLinkToFile}, followFilter):
+    echo ed2kLink(p)
+
+
+
+var
+  isRecursive = false
+  paths = newSeq[string]()
+  displayHelp = false
+
+
+var
+  opts = initOptParser()
+while true:
+  opts.next()
+  case opts.kind
+  of cmdEnd:
+    break
+  of cmdShortOption, cmdLongOption:
+    case opts.key
+    of "r", "recursive":
+      isRecursive = true
+    of "h", "help":
+      displayHelp = true
+    else:
+      echo "Unknown option: " & opts.key
+      quit()
+  of cmdArgument:
+    paths &= opts.key
+
+
+if displayHelp or paths.len == 0:
   printHelp()
+else:
+  for path in paths:
+    if fileExists(path):
+      handleFile(path)
+    elif dirExists(path):
+      handleDir(path, isRecursive)
+    else:
+      echo "Invalid path '" & path & "'"
